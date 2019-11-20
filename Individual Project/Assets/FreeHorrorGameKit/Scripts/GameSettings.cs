173ca3a6c46f085bc1732543f96ec0c84719e1db
Andrew <andrew.s.roth@vanderbilt.edu>
2019-11-20 17:04:05 -0600

    woooo

--- Individual Project/Assets/FreeHorrorGameKit/Scripts/GameSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿public class GameSettings
-{
-    public bool fullscren;
-    public int textureQuality;
-    public int antialiasing;
-    public int vSync;
-    public int resolutionsIndex;
-    public float musicVolume;
-}
